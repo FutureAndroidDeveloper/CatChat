7a1a586ba0e08abe04b47d6e4e21748fa691e5b4
Kirill Klimenkov <dev.kirill.klimenkov@gmail.com>
2018-11-15 22:51:02 +0300

    Added call button navigation panel

--- app/src/main/java/com/example/kirill/catchat/MainActivity.java
+++ app/src/main/java/com/example/kirill/catchat/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -28,6 +29,12 @@ public class MainActivity extends AppCompatActivity
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.content_frame, fragment);
         fragmentTransaction.commit();
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open_drawer, R.string.nav_close_drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     @Override
