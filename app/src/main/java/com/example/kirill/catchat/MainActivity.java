65593f7c8b05a6da75161337b175a2fb303b253b
Kirill Klimenkov <dev.kirill.klimenkov@gmail.com>
2018-11-15 22:41:12 +0300

    Realized listener for navigation panel

--- app/src/main/java/com/example/kirill/catchat/MainActivity.java
+++ app/src/main/java/com/example/kirill/catchat/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.kirill.catchat;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,54 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.content_frame, fragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        Fragment fragment = null;
+        Intent intent = null;
+
+        switch (id) {
+            case R.id.nav_inbox: {
+                fragment = new InboxFragment();
+                break;
+            }
+            case R.id.nav_drafts: {
+                fragment = new DraftsFragment();
+                break;
+            }
+            case R.id.nav_sent: {
+                fragment = new SentItemsFragment();
+                break;
+            }
+            case R.id.nav_trash: {
+                fragment = new TrashFragment();
+                break;
+            }
+            case R.id.nav_help: {
+                intent = new Intent(this, HelpActivity.class);
+                break;
+            }
+            case R.id.nav_feedback: {
+                intent = new Intent(this, FeedbackActivity.class);
+                break;
+            }
+            default:
+                fragment = new InboxFragment();
+        }
+
+        if (fragment != null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.content_frame, fragment);
+            transaction.commit();
+        } else {
+            startActivity(intent);
+        }
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START, true); // ?????????
+
+        return true;
     }
 }
